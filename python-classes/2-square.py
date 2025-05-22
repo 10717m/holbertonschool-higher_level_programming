1bfea002d251be13b941a5c04a1939c5b0b126fa
mohammed <mohammedbz670@gmail.com>
2025-05-22 10:23:51 +0000

    p

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""Defines a square with size validation."""
+"""declare a private class attribute (variable) to return size"""
 
 
 class Square:
-    """Represents a square with size validation."""
+    """a class to represent Square"""
+    __size = 0
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """an instance variable to accept the size with validating input"""
 
-        Args:
-            size (int): The size of the new square. Defaults to 0.
-        
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
