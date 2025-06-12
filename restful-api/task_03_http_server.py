c942e93dedde1fcf9d8749af8e8fe71a3c4d5d72
mohammed <mohammedbz670@gmail.com>
2025-06-12 10:07:57 +0000

    p

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,7 +3,6 @@ import http.server
 import json
 from http import HTTPStatus
 
-
 class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
@@ -22,16 +21,8 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
         elif self.path == '/status':
             self.send_response(HTTPStatus.OK)
             self.send_header("Content-type", "text/plain")
-            self.send_header("Content-Length", str(len("OK")))
             self.end_headers()
             self.wfile.write(b"OK")
-
-        elif self.path == '/info':
-            self.send_response(HTTPStatus.OK)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            info = {"version": "1.0", "description": "A simple API built with http.server"}
-            self.wfile.write(json.dumps(info).encode('utf-8'))
 
         else:
             self.send_response(HTTPStatus.NOT_FOUND)
@@ -40,46 +31,12 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             error_response = {"error": "Endpoint not found"}
             self.wfile.write(json.dumps(error_response).encode('utf-8'))
 
-    def do_POST(self):
-        if self.path == '/echo':
-            content_length = int(self.headers.get('Content-Length', 0))
-            content_type = self.headers.get('Content-Type')
-
-            if content_type != 'application/json':
-                self.send_response(HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps({"error": "Content-Type must be application/json"}).encode('utf-8'))
-                return
-
-            try:
-                body = self.rfile.read(content_length)
-                data = json.loads(body)
-            except json.JSONDecodeError:
-                self.send_response(HTTPStatus.BAD_REQUEST)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps({"error": "Invalid JSON"}).encode('utf-8'))
-                return
-
-            self.send_response(HTTPStatus.OK)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"received": data}).encode('utf-8'))
-        else:
-            self.send_response(HTTPStatus.NOT_FOUND)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"error": "Endpoint not found"}).encode('utf-8'))
-
-
 def run(server_class=http.server.HTTPServer, handler_class=SimpleAPIHandler):
     port = 8000
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
-    print(f"Serving on http://localhost:{port}")
     httpd.serve_forever()
-
 
 if __name__ == "__main__":
     run()
+
