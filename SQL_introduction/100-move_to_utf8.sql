a60a652437c9d10ad3a47124dbce2b76add44468
mohammed <mohammedbz670@gmail.com>
2025-06-24 13:08:13 +0000

    sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,12 @@
 -- Convert database to utf8mb4
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
--- Convert table to utf8mb4
+-- Use the database
 USE hbtn_0c_0;
+
+-- Convert table to utf8mb4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert column 'name' to utf8mb4
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Explicitly change column `name` with only COLLATE (no CHARACTER SET)
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
 
