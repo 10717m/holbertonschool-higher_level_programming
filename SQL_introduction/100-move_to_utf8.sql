0ac424d3d7abd670a4eb7ecd93b49ed750aca11b
mohammed <mohammedbz670@gmail.com>
2025-06-24 13:02:43 +0000

    sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,10 @@
--- Convert the database charset and collation
-ALTER DATABASE hbtn_0c_0
-  CHARACTER SET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+-- Convert database to utf8mb4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
--- Select the database to work on
+-- Convert table to utf8mb4
 USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table charset and collation
-ALTER TABLE first_table
-  CONVERT TO CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
--- Modify the 'name' column charset and collation
-ALTER TABLE first_table
-  MODIFY name VARCHAR(256)
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
+-- Convert column 'name' to utf8mb4
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
