2b9e6395831894e456ab196721371e4460b0fd86
mohammed <mohammedbz670@gmail.com>
2025-07-11 15:23:47 +0000

    java

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const texts = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(texts.join('\n'));
-2-arguments.js
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
